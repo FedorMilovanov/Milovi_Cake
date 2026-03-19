9588d9f96476a711e700291bfdd6330c2fe06d94
FedorMilovanov <viktorcoy2012@gmail.com>
2026-03-19 03:59:11 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    ══════════════════════════════════════════════ */
 
 // Version is updated on each deploy — change this date to bust caches
-const DEPLOY_VERSION = '2026-03-03d';
+const DEPLOY_VERSION = '2026-03-19a';
 const STATIC_CACHE = `milovi-static-v1-${DEPLOY_VERSION}`;
 const IMAGE_CACHE  = `milovi-images-v2`;
 
